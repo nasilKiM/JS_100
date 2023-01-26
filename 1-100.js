9939251b8ef7488e9f8a70aa8d5a6f266601436c
nasilKiM <nasikun@naver.com>
2023-01-26 21:25:11 +0900

    22번까지 문제풀이 추가

--- 1-100.js
+++ 1-100.js
@@ -1,10 +1,9 @@
 // 1번. 배열의 삭제
 // 다음 배열에서 400, 500을 삭제하는 코드를 입력하세요.
 
-let nums = [100,200,300,400,500];
-nums.splice(3,2);
+let nums = [100, 200, 300, 400, 500];
+nums.splice(3, 2);
 console.log(nums);
-
 
 // =========================================================================
 
@@ -12,33 +11,31 @@ console.log(nums);
 // <pass>부분에 배열 내장함수를 이용하여 코드를 입력하고 다음과 같이 출력되게 하세요.
 //출력 [200,100,10000,300]
 
-let arr = [200,100,300];
-arr.splice(2,0,10000);
+let arr = [200, 100, 300];
+arr.splice(2, 0, 10000);
 console.log(arr);
-
 
 // =========================================================================
 
 // 3번. 변수의 타입
 // 다음 출력 값으로 올바른 것은? : 4번(object)
 
-let arr2 = [100,200,300];
-console.log(typeof(arr2));
+let arr2 = [100, 200, 300];
+console.log(typeof arr2);
 
 // =========================================================================
 
 // 4번. 변수의 타입2
 // 다음 변수 a를 typeof(a)로 넣었을 때 출력될 값과의 연결이 맞지 않은 것은? : 2번
 
-a0=1;
-console.log(typeof(a0));
+a0 = 1;
+console.log(typeof a0);
 a1 = 2.22;
-console.log(typeof(a1));
-a2 = 'p';
-console.log(typeof(a2));
-a3 = [1,2,3]
-console.log(typeof(a3));
-
+console.log(typeof a1);
+a2 = "p";
+console.log(typeof a2);
+a3 = [1, 2, 3];
+console.log(typeof a3);
 
 // =========================================================================
 
@@ -48,10 +45,10 @@ console.log(typeof(a3));
 let a = 10;
 let b = 2;
 
-for(let i = 1; i<5; i+=2){
-    a += i;
+for (let i = 1; i < 5; i += 2) {
+  a += i;
 }
-console.log(a+b); //16
+console.log(a + b); //16
 
 /*
         a       i       a+= i
@@ -62,7 +59,6 @@ console.log(a+b); //16
 4
 
 */
-
 
 // =========================================================================
 
@@ -114,15 +110,14 @@ truthy and falsy 한 자료형들~
 // (출력값은 공백을 넣지않습니다.)
 
 let d = {
-    'height' : 180,
-    'weight' : 78,
-    'weight' : 84,
-    'temperature' : 36,
-    'eyesight' : 1
+  height: 180,
+  weight: 78,
+  weight: 84,
+  temperature: 36,
+  eyesight: 1,
 };
 
-console.log(d['weight']); // 84 
-
+console.log(d["weight"]); // 84
 
 // =========================================================================
 
@@ -130,19 +125,18 @@ console.log(d['weight']); // 84
 // 다음 소스코드를 완성하여 날짜와 시간을 출력하시오
 
 //데이터
-let year = '2019';
-let month = '04';
-let day = '26';
-let hour = '11';
-let minute = '34';
-let second = '27';
+let year = "2019";
+let month = "04";
+let day = "26";
+let hour = "11";
+let minute = "34";
+let second = "27";
 
-let result = year.concat('/',month,'/', day," ",hour,':', minute,':', second)
+let result = year.concat("/", month, "/", day, " ", hour, ":", minute, ":", second);
 // 빈칸을 채워주세요.
 
 console.log(result);
 //출력 예시 : 2019/04/26 11:34:27
-
 
 // =========================================================================
 
@@ -152,8 +146,8 @@ console.log(result);
 let s = 0;
 //pass
 
-for(let i=0; i<100; i++){
-    s += i;
+for (let i = 0; i < 100; i++) {
+  s += i;
 }
 console.log(s);
 
@@ -167,24 +161,23 @@ console.log(s);
 //<여기에 class를 작성하세요.>
 
 const Wizard = class Wizard {
-    constructor(health, mana, armor){
+  constructor(health, mana, armor) {
     this.health = health;
     this.mana = mana;
     this.armor = armor;
-}
-attack(){
-    console.log('파이어볼');
-}
-}
+  }
+  attack() {
+    console.log("파이어볼");
+  }
+};
 
 const x = new Wizard(545, 210, 10);
-console.log('wizard의 능력은 ' +  x.health, x.mana, x.armor);
+console.log("wizard의 능력은 " + x.health, x.mana, x.armor);
 x.attack();
 
 //출력
 // 545 210 10
 //파이어볼
-
 
 /* class관련 공부
 
@@ -243,7 +236,6 @@ console.log(planet[n-1]);
 >> prompt로 사용자의 입력값을 받는걸 깜빡잊고있었다...
 */
 
-
 // =========================================================================
 
 // 14번. 3의 배수인가요?
@@ -252,16 +244,15 @@ console.log(planet[n-1]);
 // 입력으로 랜덤한 숫자 n이 주어집니다.
 // 만약 그 수가 3의 배수라면 '짝'이라는 글자를, 3의 배수가 아니라면 n을 그대로 출력해주세요.
 
-//한글설계 
+//한글설계
 // (1) 3의 배수를 알 수 있는 건? : n / 3  나머지 값이 0임.
 
 let n;
-if (n%3 == 0) {
-    console.log("짝");
-}else {
-    console.log(n);
+if (n % 3 == 0) {
+  console.log("짝");
+} else {
+  console.log(n);
 }
-
 
 // =========================================================================
 
@@ -270,12 +261,12 @@ if (n%3 == 0) {
 // 만약 입력으로 '김다정'이라는 이름이 주어지면 "안녕하세요. 저는 김다정입니다."
 // 라고 출력하게 해주세요.
 
-const introduce = prompt("자기소개를 합니다")
+const introduce = prompt("자기소개를 합니다");
 
-if(introduce == "김다정") {
-    console.log("안녕하세요. 저는 김다정입니다.");
+if (introduce == "김다정") {
+  console.log("안녕하세요. 저는 김다정입니다.");
 } else {
-    console.log("반갑습니다.");
+  console.log("반갑습니다.");
 }
 
 // =========================================================================
@@ -283,13 +274,13 @@ if(introduce == "김다정") {
 // 16번. 로꾸거
 // 문장이 입력되면 거꾸로 출력하는 프로그램을 만들어봅시다.
 
-const text = prompt('문장을 입력하세요.');
+const text = prompt("문장을 입력하세요.");
 function reverse(str) {
-    let result = '';
-    for(let i = str.length-1; i>=0; i--){
-        result = result + str[i];
-    }
-    return result;
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result = result + str[i];
+  }
+  return result;
 }
 console.log(reverse(text));
 
@@ -302,29 +293,29 @@ console.log(reverse(text));
 // 입력으로 키가 주어지면
 // 키가 150이 넘으면 YES를 틀리면 NO를 출력하는 프로그램을 작성하세요.
 
-const height = prompt('키가 어떻게 되세요?');
-if(height >= 150){
-    console.log(YES);
-}else {
-    console.log(NO);
+const height = prompt("키가 어떻게 되세요?");
+if (height >= 150) {
+  console.log(YES);
+} else {
+  console.log(NO);
 }
 
 // =========================================================================
 
 // 18번. 평균 점수
 // 영하네 반은 국어, 수학, 영어 시험을 보았습니다. 영하는 친구들의 평균 점수를 구해주기로 했습니다.
-// 공백으로 구분하여 세 과목의 점수가 주어지면 
-// 전체 평균 점수를 구하는 프로그램을 작성하세요. 
+// 공백으로 구분하여 세 과목의 점수가 주어지면
+// 전체 평균 점수를 구하는 프로그램을 작성하세요.
 // 단, 소숫점 자리는 모두 버립니다.
 
-const subjectScore = prompt('국,영,수 시험성적을 차례로 입력하세요').split(" ");
+const subjectScore = prompt("국,영,수 시험성적을 차례로 입력하세요").split(" ");
 // 세 과목의 점수를 입력받고 빈칸을 기준으로 구분한다.
 let sum = 0;
-for(let i=0; i<3; i++) {
-    sum += parseInt(subjectScore[i], 10);
+for (let i = 0; i < 3; i++) {
+  sum += parseInt(subjectScore[i], 10);
 }
 //parseInt는 string을 number로 만든다. parseInt(string, n진수);
-console.log(Math.floor(sum/3));
+console.log(Math.floor(sum / 3));
 
 // 그리고 sum 변수에 for 문을 돌려서 각 변수의 값들의 데이터 타입을
 // 10진수 형태의 숫자로 변환하고 그 값을 더해준다.
@@ -335,8 +326,8 @@ console.log(Math.floor(sum/3));
 // 19번. 제곱을 구하자
 // 공백으로 구분하여 두 숫자 a, b가 주어지면 a의 b승을 구하는 프로그램을 작성
 
-let 제곱 = prompt('숫자 두개를 입력하세요').split("");
-console.log(Math.pow(parseInt(제곱[0],10),parseInt(제곱[1],10)));
+let 제곱 = prompt("숫자 두개를 입력하세요").split("");
+console.log(Math.pow(parseInt(제곱[0], 10), parseInt(제곱[1], 10)));
 // Math.pow(a,b) -> a의 b제곱을 구해준다.
 
 // =========================================================================
@@ -345,6 +336,27 @@ console.log(Math.pow(parseInt(제곱[0],10),parseInt(제곱[1],10)));
 // 공백으로 구분하여 두 숫자가 주어집니다.
 // 두번째 숫자로 첫번째 숫자를 나누었을 떄 그 몫과 나머지를 공백으로 구분하여 출력
 
-const number = prompt('숫자 두개를 입력하세요')
-const numbers = number.split(' ')
-console.log(numbers[0]/numbers[1] , numbers[0]%numbers[1] )
+const number = prompt("숫자 두개를 입력하세요");
+const numbers = number.split(" ");
+console.log(numbers[0] / numbers[1], numbers[0] % numbers[1]);
+
+// =========================================================================
+
+// 21번. set은 어떻게만드나요?
+// 다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+/*
+1) let x = {1,2,3,5,6,7}
+2) let x = {}
+3) let x = new Set('javascript');
+4) let x = new Set(range(5));
+5) let x = new Set();
+*/
+
+// =========================================================================
+
+// 22번. 배수인지 확인하기
+// 다음 중 변수 i 가 6의 배수인지 확인하는 방법으로 올바른 것은?
+/*
+
+
+*/
